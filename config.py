30e016e6f18b45a383af89321e260c406060eb51
KIN <user.ctf@yandex.ru>
2017-12-16 18:55:12 +0300

    First adding

---
+++ config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 17:38:58 2017
+
+@author: Карпов Иван
+"""
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 17:38:38 2017
+
+@author: Карпов Иван
+"""
+
